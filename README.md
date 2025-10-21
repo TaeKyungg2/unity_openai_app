6d24fc46f866627cd5eaa278b6f21ef715df9dd6
Taery <tae5431@gmail.com>
2025-10-21 13:28:59 +0900

    asdf

---
+++ README.md
@@ -0,0 +1,4 @@
+## unity in openai
+<img src="assets/image.png" width="80%">
+openai app sdk 에 unity 의 3d 를 넣는 프로젝트입니다. 
+three.js, gltf 포맷이 사용됩니다.
--- my.js
+++ my.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 console.log("hello?");
 const loader = new GLTFLoader();
 let mixer, actions = {}, currentAction;
